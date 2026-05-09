8fade7a8b005d0fd31ae0a1ffdcf71be54e602ef
Shreyash kawde <shreyashkawde0@gmail.com>
2026-05-09 16:18:51 +0530

    ...

--- Backend/main.py
+++ Backend/main.py
@@ -5,6 +5,13 @@ app = FastAPI()
 
 app.include_router(router, prefix="/Backend")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def get():
