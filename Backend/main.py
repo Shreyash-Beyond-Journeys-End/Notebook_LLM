c102e177341ef8fe61f25c5251004b4a873fef64
Shreyash kawde <shreyashkawde0@gmail.com>
2026-05-09 16:51:33 +0530

    ..
    Removed CORS middleware configuration from FastAPI app.

--- Backend/main.py
+++ Backend/main.py
@@ -1,18 +1,12 @@
 from fastapi import FastAPI
 from app.routes.NoteBook import router
-from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
 app.include_router(router, prefix="/Backend")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 @app.get('/')
 def get():
